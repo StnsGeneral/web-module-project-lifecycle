61d9a93e43e5b74e805cb299bb63213fd7853b5d
Tristan Miller <tristanmiller89@gmail.com>
2021-05-09 22:44:41 -0500

    added more user information

--- lifecycle/src/components/User.jsx
+++ lifecycle/src/components/User.jsx
@@ -9,6 +9,20 @@ class User extends React.Component {
           Current username you are looking at is:{' '}
           <a href={this.props.user.html_url}>{this.props.user.login}</a>
         </h2>
+        {this.props.user.bio && <p>About: {this.props.user.bio}</p>}
+        {this.props.user.location && (
+          <p>Location: {this.props.user.location}</p>
+        )}
+        {this.props.user.public_repos && (
+          <p>Repos: {this.props.user.public_repos}</p>
+        )}
+        {this.props.user.following && (
+          <p>Following: {this.props.user.following}</p>
+        )}
+        {this.props.user.followers && (
+          <p>Followers: {this.props.user.followers}</p>
+        )}
+
         <Followers followers={this.props.followers} />
       </div>
     );
